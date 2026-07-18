d06599b135ec5a7df8283eb7fb366e8bf9c1826f
luobo <luobochuanqi@qq.com>
2026-07-19 00:41:15 +0800

    fix: 移除未使用的 FC 和 StrictMode 类型导入

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { type FC } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuthStore } from './store/authStore'
 import LoginPage from './pages/LoginPage'
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { type StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
